8214923722f7c032044476afb6624bd6ff06ecf0
duncanfun <u101530009@gap.kmu.edu.tw>
2026-04-10 18:25:57 +0800

    完成資料庫建立與neon連線

--- django_line/bot/models.py
+++ django_line/bot/models.py
@@ -1,3 +1,19 @@
+#python manage.py makemigrations
+#python manage.py migrate
 from django.db import models
 
 # Create your models here.
+
+class MemberInfo(models.Model):
+    line_id = models.CharField(max_length=100, unique=True, db_index=True) #line ID
+    phone = models.CharField(max_length=30, blank=True) #姓名
+    email = models.EmailField(blank=True) #E-mail
+    graduate_year = models.IntegerField(null=True, blank=True, db_index=True) #畢業年
+    is_blocked = models.BooleanField(default=False) #是否封鎖，封鎖就不傳送，預設否
+    created_at = models.DateTimeField(auto_now_add=True) #資料建立時間
+
+    def __str__(self):
+        return self.line_id
+
+    class Meta:
+        db_table = "member_info"
---
+++ note.md
@@ -0,0 +1,11 @@
+# 開發的筆記
+
+### 常用cmd語法
+#### 變更資料庫設計時
+python manage.py makemigrations
+python manage.py migrate
+
+### 相依套件
+pip install psycopg
+pip install dj-database-url==1.3.0
+pip install psycopg2
