0c50a4137ebd8b75555dbd50c4fb61a2668055db
duncanfun <u101530009@gap.kmu.edu.tw>
2026-04-16 19:08:56 +0800

    admin打不開是python版本問題，恢復修改前

--- bot/admin.py
+++ bot/admin.py
@@ -3,4 +3,8 @@ from .models import MemberInfo
 
 # Register your models here.
 
-admin.site.register(MemberInfo)
+@admin.register(MemberInfo)
+class MemberInfoAdmin(admin.ModelAdmin):
+    list_display = ("line_id", "name", "phone", "email", "graduate_year", "is_blocked", "created_at")
+    search_fields = ("line_id", "name", "phone", "email")
+    list_filter = ("graduate_year", "is_blocked", "created_at")
