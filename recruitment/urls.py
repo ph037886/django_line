695c1f2d912cac078d80b7ef4a693dc28c953abf
duncanfun <u101530009@gap.kmu.edu.tw>
2026-05-10 22:22:32 +0800

    修改recruitment的urls，確認是不是urls的問題

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -6,12 +6,5 @@ app_name = "recruitment"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     
-
-    path(
-        "<uuid:post_uuid>/",
-        views.post_detail,
-        name="post_detail"
-    ),
-    
     path("<str:short_id>/", views.post_detail, name="post_detail"),
 ]
