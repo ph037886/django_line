e533e3afe9b1780e3ccc0d666d89a4a0bbaec442
duncanfun <u101530009@gap.kmu.edu.tw>
2026-05-12 12:07:50 +0800

    新增：models紀錄是哪一次徵才事件，用foreignkey紀錄活動日期

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MemberInfo
+from .models import MemberInfo, RecruitmentEvent
 
 # Register your models here.
 
@@ -8,3 +8,9 @@ class MemberInfoAdmin(admin.ModelAdmin):
     list_display = ("line_id", "name", "phone", "email", "graduate_year", "is_blocked", "created_at", "live_site")
     search_fields = ("line_id", "name", "phone", "email")
     list_filter = ("graduate_year", "is_blocked", "created_at", "live_site")
+    
+@admin.register(RecruitmentEvent)
+class RecruitmentEventAdmin(admin.ModelAdmin):
+    list_display=("event_date", "event_name")
+    search_fields=("event_date", "event_name")
+    list_filter=("event_date", "event_name")
--- bot/models.py
+++ bot/models.py
@@ -14,10 +14,28 @@ class MemberInfo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) #資料建立時間
     line_display_name = models.CharField(max_length=100, blank=True) #line顯示名稱
     consent_recruitment = models.BooleanField(default=False) #是否願意接受徵才訊息
-    live_site=models.CharField(max_length=20, blank=True)
-
+    live_site=models.CharField(max_length=20, blank=True) #居住地
     def __str__(self):
         return self.line_id
 
     class Meta:
         db_table = "member_info"
+        
+class RecruitmentEvent(models.Model):
+    event_id=models.AutoField(primary_key=True)
+    #event_date=models.DateField(blank=False)
+    event_date=models.ForeignKey(
+        MemberInfo,
+        on_delete=models.CASCADE
+    )
+    event_name=models.CharField(max_length=50, blank=False)
+    
+    @property
+    def parent_date(self):
+        return self.event_date.timestamp.date()
+    
+    def __str__(self):
+        return self.event_name
+    
+    class Meta:
+        db_table='recruitment_event'
