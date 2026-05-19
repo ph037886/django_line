69734264518924e5d5acf690e1bc554bbd62775e
ph037886 <77608145+ph037886@users.noreply.github.com>
2026-05-19 10:17:34 +0800

    Update admin.py
    修改：1. 增加bot資料的預排序，使用畢業年和徵才活動日期

--- bot/admin.py
+++ bot/admin.py
@@ -8,9 +8,11 @@ class MemberInfoAdmin(admin.ModelAdmin):
     list_display = ("name", "phone", "email", "graduate_year", "is_blocked", "created_at", "live_site", "have_license", "note")
     search_fields = ("name", "phone", "email")
     list_filter = ("graduate_year", "is_blocked", "created_at", "live_site", "have_license")
+    ordering = ("-graduate_year",)
     
 @admin.register(RecruitmentEvent)
 class RecruitmentEventAdmin(admin.ModelAdmin):
     list_display=("event_date", "event_name")
     search_fields=("event_date", "event_name")
     list_filter=("event_date", "event_name")
+    ordering = ("-event_date",)
